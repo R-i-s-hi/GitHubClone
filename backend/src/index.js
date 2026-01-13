d5bd9f1c980579e0901c1d2e59a45e83987eb869
Rishabh Singh <tcrr2005@gmail.com>
2026-01-13 14:23:30 +0530

    Added basic files for command operations

---
+++ backend/src/index.js
@@ -0,0 +1,65 @@
+import yargs from 'yargs';
+import {hideBin} from 'yargs/helpers';
+
+
+import initRepo from './controllers/init.js';
+import addRepo from './controllers/add.js';
+import commitRepo from './controllers/commit.js';
+import pushRepo from './controllers/push.js';
+import pullRepo from './controllers/pull.js';
+import revertRepo from './controllers/revert.js';
+
+yargs(hideBin(process.argv))
+    .command(
+        "init",
+        "Initialize a new repository",
+        {},
+        initRepo
+    )
+    .command(
+        "add <file>",
+        "Add a file to staged",
+        (yargs) => {
+            yargs.positional('file', {
+                describe: 'File to add',
+                type: 'string'
+            });
+        },
+        addRepo
+    )
+    .command(
+        "commit <msg>",
+        "Commit the staged files",
+        (yargs) => {
+            yargs.positional('msg', {
+                describe: 'commit message',
+                type: 'string'
+            });
+        },
+        commitRepo
+    )
+    .command(
+        "push",
+        "Push the committed changes",
+        {},
+        pushRepo
+    )
+    .command(
+        "pull",
+        "Pull the latest changes",
+        {},
+        pullRepo
+    )
+    .command(
+        "revert <commitID>",
+        "Revert to a specific commit",
+        (yargs) => {
+            yargs.positional('commitID', {
+                describe: 'Commit ID to revert to',
+                type: 'string'
+            });
+        },
+        revertRepo
+    )
+    .demandCommand(1, 'You need at least one command!')
+    .help().argv;
