570a0d04ba7ff94e0567236fea66e0ae5ea161c8
Rishabh Singh <tcrr2005@gmail.com>
2026-02-13 21:03:44 +0530

    Added frontend folder and initialised basic setup

---
+++ frontend/src/Routes.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react";
+import {useNavigate, useRoutes} from 'react-router-dom'
+import { useAuth } from "./authContext.jsx";
+
+import Dashboard from "./components/dashboard/Dashboard.jsx";
+import Profile from "./components/user/Profile.jsx";
+import Login from "./components/auth/Login.jsx";
+import Signup from "./components/auth/Signup.jsx";
+
+
+const ProjectRoutes = () => {
+    const {currentUser, setCurrentUser} = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        const userIdFromStorage = localStorage.getItem("userId");
+
+        if(userIdFromStorage && !currentUser){
+            setCurrentUser(userIdFromStorage);
+        }
+        // If user is not logged in and trying to access a protected route, redirect to login
+        if(!userIdFromStorage && !["/auth", "/signup"].includes(window.location.pathname)) {
+            navigate("/auth");
+        }
+
+        if(userIdFromStorage && window.location.pathname=='/auth'){
+            navigate("/");
+        }
+    }, [currentUser, navigate, setCurrentUser]);
+
+    let element = useRoutes([
+        {
+            path:"/",
+            element:<Dashboard/>
+        },
+        {
+            path:"/auth",
+            element:<Login/>
+        },
+        {
+            path:"/signup",
+            element:<Signup/>
+        },
+        {
+            path:"/profile",
+            element:<Profile/>
+        }
+    ]);
+
+    return element;
+}
+
+export default ProjectRoutes;
